0ad7f057402c785b82d09621067acc3fffbc4551
curiouspaul1 <170408025@live.unilag.edu.ng>
2023-08-23 16:57:36 +0100

    inital commit

---
+++ mock_call_pairer.py
@@ -0,0 +1,51 @@
+from flask import (
+    request, render_template,
+    Flask, session, current_app,
+    g
+)
+from flask_session import Session
+from flask_apscheduler import APScheduler
+from utils import gen_pairs, update_file, read_file
+from dotenv import load_dotenv
+from pytz import utc
+import time
+import pprint
+import os
+import requests
+
+import logging
+
+logging.basicConfig()
+logging.getLogger('apscheduler').setLevel(logging.DEBUG)
+
+
+app = Flask(__name__)
+
+
+Session(app)
+sched = APScheduler()
+sched.init_app(app)
+
+@sched.task('interval', id='refresh_pairings', minutes=1, max_instances=1)
+def refresh_pairings():
+    update_file()
+
+sched.start()
+
+@app.get('/')
+def root():
+    pairs = read_file()
+    if not pairs:
+        print('Generating pairs...')
+        pairs = read_file()
+    return render_template('index.html', pairs=pairs)
+
+
+@app.get('/refresh')
+def rfsh():
+    res = gen_pairs()
+
+    return 'ok', 200
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5050)
---
+++ utils.py
@@ -0,0 +1,84 @@
+from dotenv import load_dotenv
+import requests
+import os
+import csv
+import random
+
+load_dotenv()
+
+owner, repo = os.getenv('OWNER'), os.getenv('REPO')
+token = os.getenv('GITHUB_TOKEN')
+BASE_URL = os.getenv('GH_URL')
+
+def fetch_collaborators(page=1):
+    _path = f'repos/{owner}/{repo}/collaborators?page={page}'
+    resp = requests.get(
+        url=BASE_URL+_path,
+        headers={
+            'Authorization': f'Bearer {token}',
+            'Accept': 'application/vnd.github+json',
+            'X-GitHub-Api-Version': '2022-11-28'
+        }
+    )
+    if resp.status_code == 200:
+        print(len(resp.json()))
+        res = [
+            obj['login']
+            for obj in resp.json()
+        ]
+        # print('1', len(res))
+        return res
+    else:
+        print(resp.status_code, resp.content)
+
+def fetch_users(page=1):
+    res = []
+    ghUsers = fetch_collaborators(page)
+    toPair = []
+    # print('2', len(ghUsers))
+    with open('data.csv', 'r') as data:
+        mdata = csv.reader(data)
+        for row in mdata:
+            res.append({'gh': row[3].lower().strip(), 'wh': row[4]})
+    
+    for usr in ghUsers:
+        for name in res:
+            if usr.lower().strip() == name['gh'].lower():
+                toPair.append(name['wh'])
+
+    return toPair
+
+def gen_pairs():
+    final = []
+    pairs = []
+    for i in range(1,3):
+        final.extend(fetch_users(i))
+
+    check = []
+    while final:
+        pair = random.sample(final, k=2)
+        pairs.append(pair)
+        check.append(pair)
+        for p in pair:
+            final.remove(p)
+    return pairs
+
+def update_file():
+    with open('output.csv', 'w') as file:
+        writer = csv.writer(file)
+        writer.writerow(['PersonA', 'PersonB'])
+        _all = gen_pairs()
+        for n in _all:
+            writer.writerow(n)
+
+def read_file():
+    with open('output.csv', 'r') as data:
+        mdata = csv.reader(data)
+        res = []
+        for n in mdata:
+            if n and n[0].lower() != 'persona':
+                res.append(n)
+    return res
+
+
+update_file()
